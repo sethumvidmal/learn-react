266ca48632efc669c1b00e488bee104286a7b323
Sethum Vidmal <sethumthabrew@gmail.com>
2024-05-13 21:24:53 +0530

    adding instructor comments

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 export default function Button({
   children,
@@ -7,6 +7,10 @@ export default function Button({
   Icon,
   ...props
 }) {
+  // !!! Important:
+  // Wrap the icon with a <span className="button-icon"> to achieve the target look
+  // Also wrap the children prop with a <span>
+
   let cssClasses = `button ${mode}-button`;
 
   if (Icon) {
@@ -32,6 +36,6 @@ export default function Button({
 Button.propTypes = {
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
-  mode: PropTypes.oneOf(['filled', 'outlined']), // Assuming there are only two modes: filled and outlined
-  Icon: PropTypes.elementType
+  mode: PropTypes.oneOf(["filled", "outlined"]), // Assuming there are only two modes: filled and outlined
+  Icon: PropTypes.elementType,
 };
