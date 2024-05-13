602435703e3b4c8e11985f651869d47c6ce5e5c0
Sethum Vidmal <sethumthabrew@gmail.com>
2024-05-13 21:16:58 +0530

    Coding Exercise 15: Creating Flexible Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,54 @@
-import "./App.css";
+import Button from "./Button";
+import HomeIcon from "./HomeIcon";
+import PlusIcon from "./PlusIcon";
 
 function App() {
   return (
-    <>
-      <p>Hello world!</p>
-    </>
+    <div id="app">
+      <section>
+        <h2>Filled Button (Default)</h2>
+        <p>
+          <Button>Default</Button>
+        </p>
+        <p>
+          <Button mode="filled">Filled (Default)</Button>
+        </p>
+      </section>
+      <section>
+        <h2>Button with Outline</h2>
+        <p>
+          <Button mode="outline">Outline</Button>
+        </p>
+      </section>
+      <section>
+        <h2>Text-only Button</h2>
+        <p>
+          <Button mode="text">Text</Button>
+        </p>
+      </section>
+      <section>
+        <h2>Button with Icon</h2>
+        <p>
+          <Button Icon={HomeIcon}>Home</Button>
+        </p>
+        <p>
+          <Button Icon={PlusIcon} mode="text">
+            Add
+          </Button>
+        </p>
+      </section>
+      <section>
+        <h2>Buttons Should Support Any Props</h2>
+        <p>
+          <Button mode="filled" disabled>
+            Disabled
+          </Button>
+        </p>
+        <p>
+          <Button onClick={() => console.log("Clicked!")}>Click me</Button>
+        </p>
+      </section>
+    </div>
   );
 }
 
